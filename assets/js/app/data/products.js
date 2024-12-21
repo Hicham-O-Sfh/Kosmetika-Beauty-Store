9a6543d5c7aea99ad6de54a5b0b1b30274d417b6
hichamoussama.saffih <Hicham.O.Sfh@gmail.com>
2024-12-21 20:16:01 +0100

    Rename the Black Afgan pictures with the id-brand-name scheme

--- assets/js/app/data/products.js
+++ assets/js/app/data/products.js
@@ -266,9 +266,12 @@ export default [
     descriptionHtml:
       "<b>Cosmo® - Black Afgan</b> est un parfum masculin de <b>luxe</b> et d'<b>élégance</b>, inspiré par les riches notes de l'<b>Oud</b> 🖤. Cette fragrance <b>orientale</b> puissante et envoûtante allie des accords <b>boisés</b> et <b>épicés</b>, créant un sillage <b>intense</b> et <b>raffiné</b>. Parfait pour l'homme qui veut affirmer son style avec une touche de mystère et de distinction 🌟. Un parfum qui ne passe pas inaperçu.",
     pics: [
-      { url: "assets/img/products/black-afgan-img-main.webp", isMain: true },
-      { url: "assets/img/products/black-afgan-2.webp" },
-      { url: "assets/img/products/black-afgan-1.webp" },
+      {
+        url: "assets/img/products/13-cosmo-black-afgan-main.webp",
+        isMain: true,
+      },
+      { url: "assets/img/products/13-cosmo-black-afgan-1.webp" },
+      { url: "assets/img/products/13-cosmo-black-afgan-2.webp" },
     ],
   },
   {
